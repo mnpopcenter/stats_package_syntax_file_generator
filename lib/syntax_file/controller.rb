7f7e915067f6d3ffdb3508fb98581752d6aaa075
Dan Elbert <dan.elbert@gmail.com>
2021-03-29 15:56:34 -0500

    bump version

--- lib/syntax_file/controller.rb
+++ lib/syntax_file/controller.rb
@@ -6,7 +6,7 @@
 module SyntaxFile
   class Controller
 
-    VERSION = "1.1.3"
+    VERSION = "1.1.7"
 
     ATTR = {
       :project                    => { :req => false, :rw => 'rw', :def => '',          :yaml => true  },
